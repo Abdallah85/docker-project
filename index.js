ba45fab0361a85335224a6dc872f99a0225dc588
Abdallah Mohamed <abdallah.moh.153@gmail.com>
2024-10-28 18:25:43 +0300

    Refactor port assignment in index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 
 const app = express();
+const port = 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
-const port = 5000;
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!"`);
